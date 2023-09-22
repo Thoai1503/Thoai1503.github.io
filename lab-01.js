3a4557631c392f0d3cba929eea13634f0e446f11
Thoai1503 <vothoai1503@gmail.com>
2023-09-22 13:33:14 +0100

    vothoai

---
+++ lab-01.js
@@ -0,0 +1,21 @@
+console.log('cau 2')
+let a = 10
+console.log(a)
+
+let b = '10'
+console.log(b)
+
+a = 20
+console.log(a)
+
+const pi = 3.14
+console.log(pi)
+
+//Tao objecrt HOc sinh
+let Hocsinh =  {
+    HoTen: 'Vo Giang Thoai',
+    Tuoi: '22',
+    Diachi: 'Hoc Mon'
+}
+console.log(Hocsinh)
+
