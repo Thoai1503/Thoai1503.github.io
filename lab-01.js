a2e28b376e909fa29f8bb713dd3a864b5839207d
Thoai1503 <vothoai1503@gmail.com>
2023-09-25 13:17:45 +0100

    Thoaivo

--- lab-01.js
+++ lab-01.js
@@ -5,8 +5,6 @@ console.log(a)
 let b = '10'
 console.log(b)
 
-a = 20
-console.log(a)
 
 const pi = 3.14
 console.log(pi)
@@ -16,6 +14,49 @@ let Hocsinh =  {
     HoTen: 'Vo Giang Thoai',
     Tuoi: '22',
     Diachi: 'Hoc Mon'
+    
 }
 console.log(Hocsinh)
+//Khai bao thuoc tính Dienthoai
+let Dienthoai = {Ten: 'iPhone 14',
+hangSanXuat: 'Apple',
+mauSac: 'Trang',
+Gia:'1000 USD'
+}
+console.log(Dienthoai)
+//Thay doi thuoc tinh gian\
+
+Dienthoai.Gia = '750 USD'
+console.log(Dienthoai)
+
+///Toan tu
+//Cau a. Khoi tao bien va lam phep tinh
+let c = 3
+let d = 4
+let KetQua = c + d
+console.log(KetQua)
+
+//Cau c...
+let x = 3
+let y = 4
+let z = 6
+let Ketqua1 = x*x + 2*x + 1
+let Ketqua2 = x*x*x - 3*x*y - 5*y + 3*y*y
+let Ketqua3 = x*y*x*y-2*x*x*y+13*y
+let Ketqua4 = 4*x*x*x+3*x*y+y*y-(2*x*x-3*y)
+let Ketqua5 = ((5*x*x)/(4*x*y))+y*y
+let Ketqua6 = x*x-((2*z*x*y)/(y*y))+5*x-2*y*y+4*x*z*z*z+z*z*z
+
+console.log('Ketqua1 =',Ketqua1)
+console.log('Ketqua2 =',Ketqua2)
+console.log('Ketqua3 =',Ketqua3)
+console.log('Ketqua4 =',Ketqua4)
+console.log('Ketqua5 =',Ketqua5)
+console.log('Ketqua6 =',Ketqua6)
+
+//Cau d Tinh chu vi va dien tich hinh tron co duong kinh la 5
+let e = 5
+const pi2 = 3.14
+let ChuViHinhTron = e*pi2
+console.log('Chu vi hinh tron',ChuViHinhTron)
 
