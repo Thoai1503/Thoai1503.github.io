0bbe404fceec0b0883b4dbfbe967aa1ce42399dd
Thoai1503 <vothoai1503@gmail.com>
2023-10-02 13:46:28 +0100

    Thoaivo

--- lab-02.js
+++ lab-02.js
@@ -55,3 +55,105 @@ kiemtrasoAmDuong(-1)
         }
   }
   soLonNhatIn3So(3225,645,432)
+
+  //Bai 2
+  let a2 = 2
+  let b2 = 3
+  let c2 = 1
+  function giaiPhuongTrinhBac2 (a2,b2,c2){
+    const delta = b2*b2-4*a2*c2
+    
+    if (a2===0 && b2===0){
+        console.log('Phuong trinh vo nghiem')
+    }if  (a2=0){
+        const x= -c/b
+        console.log('Nghiem la x=',x)
+    }if  (delta<0){
+        console.log('Phuong trinh vo nghiem')
+    }else {
+            const x1 =(-b2 + Math.sqrt(delta))/(2*a2)
+            const x2 =(-b2 - Math.sqrt(delta))/(2*a2)
+            console.log('x1=',x1 + '/ x2=',x2)
+    
+        }
+  }
+  giaiPhuongTrinhBac2(2,-5,2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  function tinhGiaCuoc(soPhut){
+    const phiCoDinh = 25000
+    let tongChiPhi = 0
+    let chiPhi =0
+    
+    if(soPhut >= 0 && soPhut <=50){
+        chiPhi = soPhut * 600
+    }
+    else if(soPhut > 50 && soPhut<=200){
+        chiPhi =50*600 + (soPhut -50)*400
+    }
+    else if (soPhut >200){
+        chiPhi =50*600 +150*400 + (soPhut-200)*200
+    }
+
+     tongChiPhi= phiCoDinh + chiPhi
+     return tongChiPhi
+}
+ 
+let soPhut =60
+let tongChiPhi = tinhGiaCuoc(soPhut)
+console.log('Gia cuoc dien thoai cho',soPhut +'phút gọi là',tongChiPhi + 'đồng')
+
+
+function tinhGiaCuocDienThoai(soPhutGoi) {
+    const phiThueBaoBatBuoc = 25000; // Phí thuê bao bắt buộc là 25,000 đồng
+    const phiPhutGoi = [600, 400, 200]; // Mảng phí cho từng khoảng phút gọi
+    const phutGoiMoiKhoang = [50, 150, Infinity]; // Mảng số phút cho từng khoảng phí
+  
+    let giaCuoc = phiThueBaoBatBuoc; // Bắt đầu với phí thuê bao bắt buộc
+  
+    for (let i = 0; i < phiPhutGoi.length; i++) {
+      if (soPhutGoi <= phutGoiMoiKhoang[i]) {
+        giaCuoc += soPhutGoi * phiPhutGoi[i];
+        break; // Kết thúc vòng lặp khi đã tính xong giá cước
+      } else {
+        giaCuoc += phutGoiMoiKhoang[i] * phiPhutGoi[i];
+        soPhutGoi -= phutGoiMoiKhoang[i]; // Giảm số phút đã tính giá cước
+      }
+    }
+  
+    return giaCuoc;
+  }
+  
+  // Sử dụng hàm để tính giá cước với số phút gọi điện thoại cụ thể.
+  const soPhutGoi = 250; // Ví dụ có 250 phút gọi
+  const giaCuoc = tinhGiaCuocDienThoai(soPhutGoi);
+  console.log(`Giá cước điện thoại cho ${soPhutGoi} phút gọi là ${giaCuoc} đồng`);
--- lab-03.js
+++ lab-03.js
@@ -3,5 +3,42 @@ let arr = [3, 451, 27]
 let doDaiMang = arr.length
 
 for(let i = 0; i < doDaiMang; i = i+1){
-    console.log('Phan tu thu % = %',i, arr[i])
+    console.log('Phan tu thu %s = %s',i, arr[i])
 }
+// Bai 1 b
+let arr1 = [3,451,5,2,6,76,3,5,1,243,6,4]
+let doDaiMang1 = arr.length
+let tongPhanTu =0
+
+for(let i=0; i<doDaiMang; i= i+1){
+    tongPhanTu = tongPhanTu + arr[i]
+    console.log('Tong cac gia tri trong mang la: %s',i,arr[i])
+
+}
+// Bai 1c
+let soLonNhat = arr1[0]
+let soNhoNhat= arr1[0]
+for (i=1;i<doDaiMang1;i=i+1){
+    if (soLonNhat < arr[i]){
+        soLonNhat = arr[i]
+    }  
+    if (soNhoNhat > arr[i]){
+        soNhoNhat = arr[i]
+    }
+    console.log('So lon nhat la',soLonNhat)
+    console.log('So nho nhat la',soNhoNhat)
+}
+// Bai 1d
+
+
+// Bai 1e
+let arr2 = []
+let j = 0
+ for(let i = doDaiMang1 - 1; i >=0; i--){
+    arr2[j] = arr1[i]
+    j++
+ }
+ console.log(arr2)
+
+
+
